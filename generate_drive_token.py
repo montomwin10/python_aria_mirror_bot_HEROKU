6e5177620a953f2721fad320b37ce128e05ef93c
montomwin10 <71692526+montomwin10@users.noreply.github.com>
2021-04-24 18:31:20 +0200

    Update generate_drive_token.py

--- generate_drive_token.py
+++ generate_drive_token.py
@@ -1,47 +1,22 @@
-from __future__ import print_function
-import os.path
-from googleapiclient.discovery import build
+import pickle
+import os
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
 
-# If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+credentials = None
+__G_DRIVE_TOKEN_FILE = "token.pickle"
+__OAUTH_SCOPE = ["https://www.googleapis.com/auth/drive"]
+if os.path.exists(__G_DRIVE_TOKEN_FILE):
+    with open(__G_DRIVE_TOKEN_FILE, 'rb') as f:
+        credentials = pickle.load(f)
+        if credentials is None or not credentials.valid:
+            if credentials and credentials.expired and credentials.refresh_token:
+                credentials.refresh(Request())
+else:
+    flow = InstalledAppFlow.from_client_secrets_file(
+        'credentials.json', __OAUTH_SCOPE)
+    credentials = flow.run_console(port=0)
 
-def main():
-    """Shows basic usage of the Gmail API.
-    Lists the user's Gmail labels.
-    """
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-
-    service = build('gmail', 'v1', credentials=creds)
-
-    # Call the Gmail API
-    results = service.users().labels().list(userId='me').execute()
-    labels = results.get('labels', [])
-
-    if not labels:
-        print('No labels found.')
-    else:
-        print('Labels:')
-        for label in labels:
-            print(label['name'])
-
-if __name__ == '__main__':
-    main()
+# Save the credentials for the next run
+with open(__G_DRIVE_TOKEN_FILE, 'wb') as token:
+    pickle.dump(credentials, token)
